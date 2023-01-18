e228c28dac43ff61640986c82c32ec33698dd088
Kevin <kstamps2484@gmail.com>
2023-01-18 07:39:48 -0700

    test next js Head component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,13 @@
 import Link from 'next/link'
 
+import Head from 'next/head'
+
 export default function FirstPost() {
     return (
         <>
+        <Head>
+            <title>First Post</title>
+        </Head>
         <h1>First Post</h1>
         <Link href="/">Back Home</Link>
         </>
